c13241d6128199139c68318ebbc8669bf9568f3d
frankie <frankie.lam54@gmail.com>
2025-03-15 17:46:27 +1300

    Added basic server side MSW

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const response = await fetch('http://localhost:3000/user')
+  const user = await response.json();
+  
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
@@ -21,7 +24,7 @@ export default function Home() {
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            {`Here is the name: ${user.name}`}
           </li>
         </ol>
 
